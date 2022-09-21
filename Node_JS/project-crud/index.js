59d90c1c1e41dcc5860cce2315969e31b7dff29a
sfjbssept <sfjbssept@gmail.com>
2022-09-21 15:22:12 +0000

    deletemethod

--- Node_JS/project-crud/index.js
+++ Node_JS/project-crud/index.js
@@ -22,10 +22,29 @@ mysqlConnection.connect((err) => {
 
 app.listen(3000, () => console.log("Express server is running at port 3000"));
 
+//get all employees
 app.get('/getemployees',(res,req) => {
     mysqlConnection.query("select * from Employee", (err, rows, feilds) => {
         if(!err) {
             console.log(rows);
         } else console.log(err);
+    })
+})
+
+// get employee by id
+app.get('/getemployee/:id', (req,res) => {
+    mysqlConnection.query("select * from Employee where EmpID= ?", [req.params.id], (err, rows,fields) => {
+        if(!err) {
+            console.log(rows);
+        }else console.log(err);
+    })
+})
+
+//delete employee by id
+app.delete("/deleteemployee/:id" , (req, res) => {
+    mysqlConnection.query("Delete from Employee where EmpID= ?" , [req.params.id], (err,rows,fields) => {
+        if(!err) {
+            res.send("employee deleted sucessfully")
+        }else console.log(err);
     })
 })
