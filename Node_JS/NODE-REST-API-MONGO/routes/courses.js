742260162f0243757eab89951dcfffbd4aa00ed3
sfjbssept <sfjbssept@gmail.com>
2022-09-26 15:24:27 +0000

    jwt

--- Node_JS/NODE-REST-API-MONGO/routes/courses.js
+++ Node_JS/NODE-REST-API-MONGO/routes/courses.js
@@ -16,11 +16,37 @@ router.get("/allcourses", async (req, res) => {
 
  router.post("/addcourse",async(req,res) => {
     try{
+      //console.log(req.body)
       const course= await Course.create(req.body);
       res.json(course);
     } catch (err) {
         res.json(err)
     }
  })
+
+ // delete method
+ router.delete("/delete/:courseId", async(req, res) => {
+    try {
+        await Course.remove({ _id: req.params.courseId });
+        res.status(200).json({
+            message: "deleted sucessfully",
+        });
+    } catch (error) {
+        res.send(error);
+    }
+ });
+
+ // update course
+
+ router.put("/update/:courseId", async (req, res) => {
+    const courseId = req.params.courseId;
+    try{
+        const course = await Course.updateOne({ _id:
+        courseId }, req.body);
+        res.json(course);         
+    }   catch (error) {
+        res.json(error);
+    }
+    });
 
 module.exports = router;
