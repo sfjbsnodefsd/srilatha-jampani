49e24e775194e9ebb9144d325939bbd65cd91ef0
sfjbssept <sfjbssept@gmail.com>
2022-09-27 13:50:17 +0000

    api

---
+++ JWT-Auth-Project/api/users/user.service.js
@@ -0,0 +1,28 @@
+const pool = require("../../config/database")
+
+
+
+module.exports = {
+    //if we get error it will be passed as data and callback will be null
+    //if execution is sucessful then call back will have data
+    create:(data, callback) => {
+        pool.query(
+            `insert into registration(firstName, lastName, gender, email, password, number)
+            values(?,?,?,?,?,?)`,
+            [
+                data.first_name,
+                data.last_name,
+                data.gender,
+                data.email,
+                data.password,
+                data.number,
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    return callBack(error);
+                }
+                return callBack(null, results);
+            }
+        );
+    },
+};
--- Node_JS/project-crud/index.js
+++ Node_JS/project-crud/index.js
@@ -6,79 +6,83 @@ const bodyparser = require("body-parser");
 app.use(bodyparser.json());
 
 var mysqlConnection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "employeedb",
-    multipleStatements:true
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "employeedb",
+  multipleStatements:true
 });
 
 mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("sucessful connection ");
-    } else 
-    console.log("DB connection failed ");
-    console.log(err);
+  if (!err) {
+    console.log("sucessful connection ");
+  } else console.log("DB connection failed " + JSON.stringify(err, undefined, 2));
 });
 
 app.listen(3000, () => console.log("Express server is running at port 3000"));
 
-//get all employees
+
+
+// get all employees
 app.get('/getemployees',(req,res) => {
     mysqlConnection.query("select * from Employee", (err, rows, feilds) => {
+        if(!err) {
+           res.send(rows);
+        } else console.log(err);
+    } )
+})
+// get employee by id 
+app.get('/getemployee/:id' , (req,res) => {
+    mysqlConnection.query("select * from Employee where EmpID = ?", [req.params.id], (err, rows, fields) => {
         if(!err) {
             res.send(rows);
         } else console.log(err);
     })
 })
 
-// get employee by id
-app.get('/getemployee/:id', (req,res) => {
-    mysqlConnection.query("select * from Employee where EmpID= ?", [req.params.id], (err, rows,fields) => {
-        if(!err) {
-            res.send(rows);
-        }else console.log(err);
-    })
-})
 
-//delete employee by id
+//delete employee by id 
 app.delete("/deleteemployee/:id" , (req, res) => {
-    mysqlConnection.query("Delete from Employee where EmpID= ?" , [req.params.id], (err,rows,fields) => {
+    mysqlConnection.query("DELETE FROM Employee WHERE EmpID = ?" , [req.params.id], (err,rows,fields) => {
         if(!err) {
-            res.send("employee deleted sucessfully")
-        }else console.log(err);
+           res.send("employee deleted sucessfully")
+        } else console.log(err);
     })
-})
-
-
+});
 
 //insert an employee
-app.post("/addemployee", (req,res) => {
+app.post("/addemployee" , (req, res) => {
     let emp = req.body;
     var sql = "SET @EmpID = ?; SET @Name = ?; SET @EmpCode = ?; SET @Salary = ?; \
     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql,[emp.EmpID,emp.Name,emp.EmpCode,emp.Salary] , (err,rows,fields) => {
+    mysqlConnection.query(sql,[emp.EmpID, emp.Name, emp.EmpCode, emp.Salary] , (err,rows,fields) => {
         if(!err) rows.forEach(element => {
             if(element.constructor == Array)
-            res.send("Employee Id of the inserted employee is : " +element[0].EmpID);            
+            res.send("Employee Id of the inserted employee is : " +element[0].EmpID);
         });
         //    res.send(rows)
-        
-        else console.log(err);
+         else console.log(err);
     })
 });
 
 
-// update employee
-app.put("/updateemployee", (req, res) => {
+// update employee 
+app.put("/updateemployee" , (req, res) => {
     let emp = req.body;
     var sql = "SET @EmpID = ?; SET @Name = ?; SET @EmpCode = ?; SET @Salary = ?; \
     CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql,[emp.EmpID,emp.Name,emp.EmpCode,emp.Salary] , (err,rows,fields) => {
+    mysqlConnection.query(sql,[emp.EmpID, emp.Name, emp.EmpCode, emp.Salary] , (err,rows,fields) => {
         if(!err) 
-            res.send("Employee updated successfully")            
-            //    res.send(rows)
-        
-        else console.log(err);
+        res.send("Employee updated sucessfully")
+        //    res.send(rows)
+         else console.log(err);
     })
 });
+
+
+
+
+
+
+
+ 
