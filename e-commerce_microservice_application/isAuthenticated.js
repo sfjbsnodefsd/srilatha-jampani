adac34b2590ad1bbd1988bd9c533ca680e2a3277
sfjbssept <sfjbssept@gmail.com>
2022-10-03 13:15:45 +0000

    midile ware

---
+++ e-commerce_microservice_application/isAuthenticated.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken")
+
+
+export async function isAuthenticated(req, res , next) {
+  // Bearer <token>
+  const token = req.headers["authorization"].split(" ")[1];
+  
+  jwt.verify(token, "secret", (err, user) => {
+    if (err) {
+        return res.json({
+            sucess: 0,
+            message: "Unauthoroized",
+        });
+    } else {
+        req.user = user;
+        next();
+    }
+  });
+}
